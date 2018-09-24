4448d9341f2afb62cfc211a414ec57cdb210e71d
regisGRB <regis.grumberg@gmail.com>
2018-09-24 15:28:26 +0200

    24/09 15:28

--- Nouveau Formulaire/Pvp/Connexion.php
+++ Nouveau Formulaire/Pvp/Connexion.php
@@ -8,17 +8,16 @@ if(isset($_POST['connect'])) {
    $mail = $_POST['mail'];
    $mdp = sha1($_POST['mdp']);
    if(!empty($mail) AND !empty($mdp)) {
-      $requser = $bdh->prepare("SELECT * FROM kurisu WHERE mail = ? AND mdp = ?");
+      $requser = $bdh->prepare("SELECT * FROM associations WHERE name = ? AND mdp = ?");
       $requser->execute(array($mail, $mdp));
       $userexist = $requser->rowCount();
       if($userexist == 1) {
          $userinfo = $requser->fetch();
          $_SESSION['id'] = $userinfo['id'];
-         $_SESSION['pseudo'] = $userinfo['pseudo'];
+         $_SESSION['name'] = $userinfo['name'];
          $_SESSION['mail'] = $userinfo['mail'];
-         header("Location: ../page/forum/voirforum.php?id=".$_SESSION['id']."");
       } else {
-         $erreur = "Mauvais mail ou mot de passe !";
+         $erreur = "Mauvais nom d'utilisateur ou mot de passe !";
       }
    } else {
       $erreur = "Tous les champs doivent Ãªtre complÃ©tÃ©s !";
